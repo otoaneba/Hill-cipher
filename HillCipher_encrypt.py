5a0ae72d4ea37db91b125c068a0cb87ce5c79653
Naoto Abe <naotoabe1@gmail.com>
2017-12-11 12:17:28 -0500

    change variable name

--- HillCipher_encrypt.py
+++ HillCipher_encrypt.py
@@ -2,18 +2,18 @@ import numpy as np
 
 words = 'hill'
 
-secret = [[9, 8, 5, 6],
+encrypt = [[9, 8, 5, 6],
           [6, 10, 5, 9],
           [9, 4, 8, 5],
           [6, 10, 11, 4]]
 
 cipher = ''
-length = len(secret)
-matrix = np.array(secret)
+length = len(encrypt)
+matrix = np.array(encrypt)
 arr = [ord(i) - ord('a') for i in words]
-count = 0
+row = 0
 for ch in words:
     if str.isalpha(str(ch)):
-        cipher += chr(sum(matrix[count % length] * arr) % 26 + ord('a'))
-        count += 1
+        cipher += chr(sum(matrix[row % length] * arr) % 26 + ord('a'))
+        row += 1
 print(cipher)
